9e254bc695fed1bda410cced4ceeb61702ecc550
renjun <renjun.li@thoughtworks.com>
2024-08-15 00:29:34 +0800

    Finish apptore animation

--- App/AppApp.swift
+++ App/AppApp.swift
@@ -25,7 +25,7 @@ struct AppApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Home()
         }
         .modelContainer(sharedModelContainer)
     }
--- App/AppStore/CustomCornor.swift
+++ App/AppStore/CustomCornor.swift
@@ -5,4 +5,14 @@
 //  Created by Renjun Li on 2024/8/14.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomCornor: Shape {
+    let corners: UIRectCorner
+    let radius: CGFloat
+    
+    nonisolated func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
