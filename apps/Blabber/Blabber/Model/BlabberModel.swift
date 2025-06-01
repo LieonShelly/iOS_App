75dfa2497d668cbaba39604a9678ae416a2437fd
LieonShelly <lieoncx@gmail.com>
2025-06-02 00:04:15 +0800

    Update withCheckedThrowingContinuation

--- apps/Blabber/Blabber/Model/BlabberModel.swift
+++ apps/Blabber/Blabber/Model/BlabberModel.swift
@@ -40,6 +40,8 @@ import UIKit
 class BlabberModel: ObservableObject {
   var username = ""
   var urlSession = URLSession.shared
+  private let manager = CLLocationManager()
+  private var delegate: ChatLocationDelegate?
 
   nonisolated init() {
   }
@@ -49,6 +51,32 @@ class BlabberModel: ObservableObject {
 
   /// Shares the current user's address in chat.
   func shareLocation() async throws {
+    let location: CLLocation = try await withCheckedThrowingContinuation { [weak self] continuation in
+      self?.delegate = ChatLocationDelegate(manager: manager, continuation: continuation)
+      if manager.authorizationStatus == .authorizedWhenInUse {
+        manager.startUpdatingLocation()
+      }
+    }
+    print(location.description)
+    manager.stopUpdatingLocation()
+    delegate = nil
+    let address: String = try await
+    withCheckedThrowingContinuation { continuation in
+      AddressEncoder.addressFor(location: location) { address, error in
+        switch (address, error) {
+        case (nil, let error?):
+          continuation.resume(throwing: error)
+        case (let address?, nil):
+          continuation.resume(returning: address)
+        case (nil, nil):
+          continuation.resume(throwing: "Address encoding failed")
+        case let (address?, error?):
+          continuation.resume(returning: address)
+          print(error)
+        }
+      }
+    }
+    try await say("📍 \(address)")
   }
 
   /// Does a countdown and sends the message.
