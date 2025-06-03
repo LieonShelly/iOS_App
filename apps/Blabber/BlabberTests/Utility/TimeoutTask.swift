bf3892d3ba2f93f426957637d4cc7ca0bc683a51
LieonShelly <lieoncx@gmail.com>
2025-06-03 23:38:04 +0800

    Test async task

---
+++ apps/Blabber/BlabberTests/Utility/TimeoutTask.swift
@@ -0,0 +1,51 @@
+//
+//  TimeoutTask.swift
+//  BlabberTests
+//
+//  Created by Renjun Li on 2025/6/3.
+//
+
+import Foundation
+
+class TimeoutTask<Success> {
+  let seconds: Int
+  let operation: @Sendable () async throws -> Success
+  
+  init(seconds: Int, operation: @escaping @Sendable () async throws -> Success) {
+    self.seconds = seconds
+    self.operation = operation
+  }
+  
+  private var continuation: CheckedContinuation<Success, Error>?
+  
+  var value: Success {
+    get async throws {
+      try await withCheckedThrowingContinuation { continuation in
+        self.continuation = continuation
+        Task {
+          try await Task.sleep(for: .seconds(seconds))
+          self.continuation?.resume(throwing: TimeoutError())
+          self.continuation = nil
+        }
+        Task {
+          let result = try await operation()
+          self.continuation?.resume(returning: result)
+          self.continuation = nil
+        }
+      }
+    }
+  }
+  
+  func cancel() {
+    continuation?.resume(throwing: CancellationError())
+    continuation = nil
+  }
+}
+
+extension TimeoutTask {
+  struct TimeoutError: LocalizedError {
+    var errorDescription: String? {
+      return "The operation timed out"
+    }
+  }
+}
